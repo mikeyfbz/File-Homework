e2458d3dfc65bd98e008c63f6ad49367eab251a7
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-06-25 16:09:30 +0100

    setup

---
+++ src/main/java/com/codeclan/example/filesHomework/FilesHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filesHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesHomeworkApplication.class, args);
+	}
+
+}
